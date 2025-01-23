54e1b968e31c2b3890271f6ade6307f8898682f1
Daniel <dannesart@live.com>
2025-01-24 00:45:10 +0100

    splash login

--- components/atoms/Button.vue
+++ components/atoms/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="bg-rose-200 hover:bg-rose-300 text-rose-950 font-bold px-6 py-3 rounded-full"
+    class="bg-rose-200 hover:bg-rose-300 text-rose-950 font-bold px-6 py-4 rounded-full"
   >
     <slot />
   </button>
--- pages/log-in.vue
+++ pages/log-in.vue
@@ -1,14 +1,24 @@
 <template>
-  <NuxtLayout>
-    <div class="grid gap-8">
-      <h2 class="text-6xl text-blue-950 font-extrabold">Log in</h2>
+  <div
+    class="p-10 flex flex-col justify-between h-screen bg-[url(~/assets/splash.jpg)] bg-cover relative"
+  >
+    <header>
+      <div class="text-4xl text-white font-extrabold">Hogar</div>
+    </header>
 
-      <div class="flex gap-6">
+    <div class="grid gap-8 relative z-10">
+      <h2 class="text-2xl text-white font-extrabold">Log in</h2>
+
+      <div class="flex flex-col gap-6 text-white justify-center">
         <AtomsButton @click="social('google')">Google</AtomsButton>
         <AtomsButton @click="social('github')">Github</AtomsButton>
       </div>
     </div>
-  </NuxtLayout>
+
+    <div
+      class="bottom-0 absolute bg-gradient-to-t to-transparent from-black/40 h-96 left-0 right-0"
+    ></div>
+  </div>
 </template>
 
 <script setup lang="ts">
