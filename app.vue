b6738b50ede3fc5334fcfc35c33d3553ba5f6425
dannesart <dannesart@live.com>
2025-01-24 23:51:29 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,23 @@
 <template>
   <NuxtPage />
 </template>
+<script setup>
+useHead({
+  title: "Hogar",
+  meta: [
+    {
+      name: "apple-mobile-web-app-capable",
+      content: "yes",
+    },
+    {
+      name: "mobile-web-app-capable",
+      content: "yes",
+    },
+    {
+      name: "viewport",
+      content:
+        "minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+    },
+  ],
+});
+</script>
