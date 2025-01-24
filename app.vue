66cf9a1f0a6fd2e6ee84479b22c2525580b2f88f
dannesart <dannesart@live.com>
2025-01-24 01:19:25 +0100

    Adding more meta

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
 <Head>
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="mobile-web-app-capable" content="yes">
 </Head>
