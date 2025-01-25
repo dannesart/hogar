ffd668db1a240061a2354c108048d7c4e87241ef
dannesart <dannesart@live.com>
2025-01-25 02:56:33 +0100

    Update home.vue

--- pages/home.vue
+++ pages/home.vue
@@ -3,7 +3,7 @@
     <h1 class="text-6xl text-blue-950 font-extrabold">Your home</h1>
   </NuxtLayout>
 </template>
-<script lang="ts">
+<script setup lang="ts">
 definePageMeta({
   middleware: "auth",
 });
