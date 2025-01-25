4a5b1ff39a8d1e79f481252a65d1b1455c5f0c63
dannesart <dannesart@live.com>
2025-01-25 04:27:02 +0100

    Update profile.vue

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,7 +1,8 @@
 <template>
   <NuxtLayout>
-    <h1 class="text-6xl text-blue-950 font-extrabold">Profile</h1>
-
+    <template v-slot:header>
+<h1 class="text-4xl text-blue-950 font-extrabold">Profile</h1>
+</template>
     <AtomsButton @click="handleLogout"> Log me out, please. </AtomsButton>
   </NuxtLayout>
 </template>
