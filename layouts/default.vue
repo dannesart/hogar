e7a7b07738565d87dd078a95eedeb8b8f4829434
Daniel <dannesart@live.com>
2025-01-26 01:27:00 +0100

    better gutters

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex flex-col h-svh">
-    <header class="p-10 flex gap-4 items-center">
+    <header class="py-5 px-5 md:px-10 flex gap-4 items-center">
       <NuxtLink :to="'/'">
         <Icon name="meteor-icons:home" size="40" />
       </NuxtLink>
       <slot name="header"></slot>
     </header>
-    <main class="px-10 flex-1">
+    <main class="px-5 md:px-10 flex-1">
       <slot />
     </main>
     <nav
-      class="sticky px-10 py-5 flex justify-between md:shadow-md md:rounded-full md:max-w-max md:min-w-96 md:bottom-10 md:self-center"
+      class="sticky bottom-0 px-5 md:px-10 py-5 flex justify-between md:shadow-md md:rounded-full md:max-w-max md:min-w-96 md:bottom-10 md:self-center"
     >
       <NuxtLink
         v-for="nav in navs"
